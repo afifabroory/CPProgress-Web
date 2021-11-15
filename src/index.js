2199367ab750646e027853eac98fea099fd6188d
afifabroory <afif.abroory@gmail.com>
2021-11-15 09:43:59 +0800

    Initial

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import { getAnalytics } from "firebase/analytics"
+import { getDatabase, ref, onValue, query, limitToLast, orderByChild, equalTo } from "firebase/database"
+import { app } from "./initialization.js"
+
+const analytics = getAnalytics(app)
+const db = getDatabase(app)
+//const db = getDatabase()
+//connectDatabaseEmulator(db, "localhost", 9000);
+
+const submissionRow = document.querySelector('#lastSubmission')
+const acSubmissionRow = document.querySelector('#lastACSubmission')
+
+
+function taskOnLoad() {
+  const dataRef = ref(db, 'submission')
+
+  const last20Query = query(dataRef, limitToLast(20))
+  onValue(last20Query, (snapshot) => {
+    const problemRef = ref(db, 'problem')
+    const submissionData = snapshot.val()
+    onValue(problemRef, (snapshot2) => {
+      let problemData = snapshot2.val()
+      
+      for (var key in submissionData) {
+        var submissionStatus = 'text-primary mr-1'
+        
+        if (submissionData[key]['status'] === 'WA') submissionStatus = 'text-danger mr-1'
+        else if (submissionData[key]['status'] === 'AC') submissionStatus = 'text-success mr-1'
+
+        let el = `<tr><td><a href="${problemData[submissionData[key]['problemID']]}">${submissionData[key]['problemID']}</a></td><td>${new Date(submissionData[key]['date']).toDateString()}</td><td class="${submissionStatus}"">${submissionData[key]['status']}</td></tr>`
+        submissionRow.insertAdjacentHTML('beforeend', el)
+      }
+    })
+  })
+
+  const last10ACQuery = query(dataRef, orderByChild("status"), equalTo("AC"))
+  onValue(last10ACQuery, (snapshot) => {
+    const submissionData = snapshot.val()
+    const problemRef = ref(db, 'problem')
+    onValue(problemRef, (snapshot2) => {
+      let problemData = snapshot2.val()
+      
+      for (var key in submissionData) {
+        var submissionStatus = 'text-success mr-1'
+
+        let el = `<tr><td><a href="${problemData[submissionData[key]['problemID']]}">${submissionData[key]['problemID']}</a></td><td>${new Date(submissionData[key]['date']).toDateString()}</td><td class="${submissionStatus}"">${submissionData[key]['status']}</td></tr>`
+        acSubmissionRow.insertAdjacentHTML('beforeend', el)
+      }
+    })
+  })
+
+  const submissionStatusRef = ref(db, 'submissionStatus')
+  onValue(submissionStatusRef, (snapshot) => {
+    const statusData = snapshot.val()
+    var donutData = {
+      labels: ['AC', 'WA', 'CTE', 'LTE', 'RTE'],
+      datasets: [{
+        data: [statusData['AC'], statusData['WA'], statusData['CTE'], statusData['LTE'], statusData['RTE']],
+        backgroundColor: ['#00a65a', '#f56954', '#ffa500', '#808080', '#ff0000'],
+      }]
+    }
+
+    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+    var pieData = donutData;
+    var pieOptions = {maintainAspectRatio: false, responsive: true}
+
+    new Chart(pieChartCanvas, {
+      type: 'pie',
+      data: pieData,
+      options: pieOptions
+    })
+  })
+}
+
+window.onload = taskOnLoad()
---
+++ src/initialization.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app"
+const app = initializeApp({
+    apiKey: "AIzaSyBaz4F6QMbLWPk4ZGelYecN1pgsd5c1ZgA",
+    authDomain: "cp-progress.firebaseapp.com",
+    projectId: "cp-progress",
+    storageBucket: "cp-progress.appspot.com",
+    messagingSenderId: "532523493921",
+    appId: "1:532523493921:web:66fdcd699f5a2fe391d160",
+    databaseURL: "https://cp-progress-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    measurementId: "G-480B9SN15Y"
+  })
+
+export {app}
