2892690df8aeac62e20f2ac52509084b8e20b4a9
afifabroory <afif.abroory@gmail.com>
2021-11-18 02:09:16 +0800

    Reverse last submission order

--- src/index.js
+++ src/index.js
@@ -20,30 +20,34 @@ function taskOnLoad() {
     const submissionData = snapshot.val()
     onValue(problemRef, (snapshot2) => {
       let problemData = snapshot2.val()
-      
+      submissionRow.innerHTML = ''
+
+      submissionData.reverse()
       for (var key in submissionData) {
         var submissionStatus = 'text-primary mr-1'
         
         if (submissionData[key]['status'] === 'WA') submissionStatus = 'text-danger mr-1'
         else if (submissionData[key]['status'] === 'AC') submissionStatus = 'text-success mr-1'
 
-        let el = `<tr><td><a href="${problemData[submissionData[key]['problemID']]}">${submissionData[key]['problemID']}</a></td><td>${new Date(submissionData[key]['date']).toDateString()}</td><td class="${submissionStatus}"">${submissionData[key]['status']}</td></tr>`
+        let el = `<tr><td><a href="${problemData[submissionData[key]['problemID']]}">${submissionData[key]['problemID']}</a></td><td>${new Date(submissionData[key]['date']).toLocaleString()}</td><td class="${submissionStatus}"">${submissionData[key]['status']}</td></tr>`
         submissionRow.insertAdjacentHTML('beforeend', el)
       }
     })
   })
 
-  const last10ACQuery = query(dataRef, orderByChild("status"), equalTo("AC"))
+  const last10ACQuery = query(dataRef, orderByChild("status"), equalTo("AC"), limitToLast(10))
   onValue(last10ACQuery, (snapshot) => {
     const submissionData = snapshot.val()
     const problemRef = ref(db, 'problem')
     onValue(problemRef, (snapshot2) => {
       let problemData = snapshot2.val()
-      
+      acSubmissionRow.innerHTML = ''
+
+      submissionData.reverse()
       for (var key in submissionData) {
         var submissionStatus = 'text-success mr-1'
 
-        let el = `<tr><td><a href="${problemData[submissionData[key]['problemID']]}">${submissionData[key]['problemID']}</a></td><td>${new Date(submissionData[key]['date']).toDateString()}</td><td class="${submissionStatus}"">${submissionData[key]['status']}</td></tr>`
+        let el = `<tr><td><a href="${problemData[submissionData[key]['problemID']]}">${submissionData[key]['problemID']}</a></td><td>${new Date(submissionData[key]['date']).toLocaleString()}</td><td class="${submissionStatus}"">${submissionData[key]['status']}</td></tr>`
         acSubmissionRow.insertAdjacentHTML('beforeend', el)
       }
     })
@@ -53,10 +57,10 @@ function taskOnLoad() {
   onValue(submissionStatusRef, (snapshot) => {
     const statusData = snapshot.val()
     var donutData = {
-      labels: ['AC', 'WA', 'CTE', 'LTE', 'RTE'],
+      labels: ['AC', 'WA', 'CTE', 'TLE', 'RTE', 'MLE'],
       datasets: [{
-        data: [statusData['AC'], statusData['WA'], statusData['CTE'], statusData['LTE'], statusData['RTE']],
-        backgroundColor: ['#00a65a', '#f56954', '#ffa500', '#808080', '#ff0000'],
+        data: [statusData['AC'], statusData['WA'], statusData['CTE'], statusData['TLE'], statusData['RTE'], statusData['MLE']],
+        backgroundColor: ['#00a65a', '#f56954', '#ffa500', '#808080', '#ff0000', '#4f4e4e'],
       }]
     }
 
